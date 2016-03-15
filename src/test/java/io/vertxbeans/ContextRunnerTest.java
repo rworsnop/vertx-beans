93c78ccabb55c8394fcf4449bf3c5541cb2fd931
Rob Worsnop <rworsnop@gmail.com>
2016-03-15 15:57:10 -0400

    Added unit tests for ContextRunner and fixed thread validation

---
+++ src/test/java/io/vertxbeans/ContextRunnerTest.java
@@ -0,0 +1,50 @@
+package io.vertxbeans;
+
+import io.vertx.core.Future;
+import io.vertx.core.Vertx;
+import org.junit.Before;
+import org.junit.Test;
+
+import java.util.List;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.TimeoutException;
+
+import static java.util.concurrent.TimeUnit.MILLISECONDS;
+import static org.hamcrest.CoreMatchers.equalTo;
+import static org.junit.Assert.assertThat;
+
+/**
+ * Created by Rob Worsnop on 3/15/16.
+ */
+public class ContextRunnerTest {
+
+    private ContextRunner contextRunner;
+
+    @Before
+    public void setup(){
+        contextRunner = new ContextRunnerImpl(Vertx.vertx());
+    }
+
+    @Test
+    public void success() throws InterruptedException, ExecutionException, TimeoutException {
+        List<String> results = contextRunner.executeBlocking(2, handler -> {
+            if (Thread.currentThread().getClass().getName().startsWith("io.vertx")){
+                handler.handle(Future.succeededFuture("OK"));
+            } else{
+                handler.handle(Future.failedFuture("Not on event loop!"));
+            }
+        }, 10, MILLISECONDS);
+        assertThat(String.join(".", results), equalTo("OK.OK"));
+    }
+
+    @Test(expected = ExecutionException.class)
+    public void failure() throws InterruptedException, ExecutionException, TimeoutException {
+        contextRunner.executeBlocking(2, handler ->
+                handler.handle(Future.failedFuture("Something bad happened")), 10, MILLISECONDS);
+    }
+
+    @Test(expected = TimeoutException.class)
+    public void timeout() throws InterruptedException, ExecutionException, TimeoutException {
+        contextRunner.executeBlocking(1, handler -> {}, 1, MILLISECONDS);
+    }
+}
